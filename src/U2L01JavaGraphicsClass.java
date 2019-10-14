2d55bd994b81e4ceb934f27d81281b263f9c49e5
lombardim <lombardim@Lombardi-12MF3Q.ellis.int>
2019-10-14 13:25:39 -0400

    switched method order

--- src/U2L01JavaGraphicsClass.java
+++ src/U2L01JavaGraphicsClass.java
@@ -5,13 +5,7 @@ import javax.swing.JPanel;
 
 public class U2L01JavaGraphicsClass extends JPanel {
 	
-	public void paintComponent(Graphics g){
-		g.drawLine(0,0,50,50);       
-		g.drawLine(50,0,50,75);
-	}
 	
-	
-
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Canvas");
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -23,6 +17,11 @@ public class U2L01JavaGraphicsClass extends JPanel {
 	    frame.setSize(300, 200);
 	    frame.setVisible(true);
 
+	}
+	
+	public void paintComponent(Graphics g){
+		g.drawLine(0,0,50,50);       
+		g.drawLine(50,0,50,75);
 	}
 
 }
